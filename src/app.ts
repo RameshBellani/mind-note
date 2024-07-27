f7feb43551d13b9e2ef64c8e237bb5b579bb733b
RameshBellani <136491880+RameshBellani@users.noreply.github.com>
2024-07-27 13:45:48 +0530

    note app working again

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import notesRouter from './routes/notes.js';
 
 const app = express();
+
+
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
+
 app.use(express.json());
 app.use('/api/notes', notesRouter);
 
