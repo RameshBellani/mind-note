deb4c73946a4e1e8e920721324791f406532b07b
RameshBellani <136491880+RameshBellani@users.noreply.github.com>
2024-07-27 16:18:37 +0530

    upadated app.ts

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import notesRouter from './routes/notes.js';
 const app = express();
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
 app.use(express.json());
 app.use('/api/notes', notesRouter);
 export default app;
